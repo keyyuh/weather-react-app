d99392a1222cc26a85a834eb9b01d6ed653c4580
keyyuh <kdoless@yahoo.com>
2022-12-30 20:01:25 -0800

    Rendering data for one forecast day

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-import WeatherIcons from "./WeatherIcons";
+import ForecastDay from "./ForecastDay";
 
-export default function Forecast() {
-  return (
-    <div className="Forecast">
-      <div className="row">
-        <div className="col-2">
-          <div className="forecastDay text-center">WED</div>
-          <div className="weather-icon">
-            <WeatherIcons code="01d" size={42} />
+export default function Forecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+  function showForecast(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="Forecast">
+        <div className="row">
+          <div className="col-2">
+            <ForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
     );
   } else {
-    const apiKey = `d2cedbb372adaa0c4843ae0962e9e2dd`;
+    const apiKey = `40b745c14eadad7b7c4e6e4bf3b70103`;
     const latitude = props.coords.lat;
     const longitude = props.coords.lon;
     const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
---
+++ src/ForecastDay.js
@@ -0,0 +1,23 @@
+import React from "react";
+import WeatherIcons from "./WeatherIcons";
+
+export default function ForecastDay(props) {
+  function day() {
+    const date = new Date(props.data.dt * 1000);
+    const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    const day = days[date.getDay()];
+    return day;
+  }
+  return (
+    <div className="forecastDay text-center">
+      {day()}
+      <div className="weather-icon text-center">
+        <WeatherIcons code={props.data.weather[0].icon} size={42} />
+      </div>
+      <div className="temps text-center">
+        <span className="maxTemp">{Math.round(props.data.temp.max)}°</span>
+        <span className="minTemp">{Math.round(props.data.temp.min)}°</span>
+      </div>
+    </div>
+  );
+}
