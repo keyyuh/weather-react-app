b88fad1521e61fd4f20f92cbb5aea0ac7c08579b
keyyuh <kdoless@yahoo.com>
2022-12-30 19:38:07 -0800

    Started process to render live forecast data

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import WeatherIcons from "./WeatherIcons";
 
@@ -11,10 +12,15 @@ export default function Forecast() {
           <div className="weather-icon">
             <WeatherIcons code="01d" size={42} />
           </div>
-          <span className="maxTemp">34°F </span>
-          <span className="minTemp">21°F</span>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = `d2cedbb372adaa0c4843ae0962e9e2dd`;
+    const latitude = props.coords.lat;
+    const longitude = props.coords.lon;
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(showForecast);
+    return null;
+  }
 }
